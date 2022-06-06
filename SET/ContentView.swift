e8258c8dcd81877b9f062ce42cf79c43859fb03c
George Mapaya <lags-twisted-0b@icloud.com>
2022-06-06 14:22:06 -0400

    Initial Commit

---
+++ SET/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SET
+//
+//  Created by George Mapaya on 2022-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SET/SETApp.swift
@@ -0,0 +1,17 @@
+//
+//  SETApp.swift
+//  SET
+//
+//  Created by George Mapaya on 2022-06-06.
+//
+
+import SwiftUI
+
+@main
+struct SETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
