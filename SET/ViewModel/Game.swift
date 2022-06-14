b820a12462839c54aaf2b3389340da3c3201a02d
George Mapaya <lags-twisted-0b@icloud.com>
2022-06-14 10:28:18 -0400

    add functionality to shuffle cards when starting a new game

--- SET/ViewModel/Game.swift
+++ SET/ViewModel/Game.swift
@@ -169,7 +169,7 @@ final class Game: ObservableObject {
             let symbolCards = createCards(for: mainSymbol)
             cards += symbolCards
         }
-        deck = cards
+        deck = cards.shuffled()
     }
     
     private func createCards(for mainSymbol: Card.MainSymbol) -> [Card] {
